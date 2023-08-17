c86a015eca5e5ec5b306a78d03658099288f710b
leesoojin <leesoojin800@gmail.com>
2023-08-17 13:23:34 +0900

    feat : react-full-page로 main화면 위아래로 슬라이드 구현

---
+++ src/components/mains/FirstMain.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function FirstMain() {
+  return <div>FirstMain</div>;
+}
+
+export default FirstMain;
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,5 +1,28 @@
 import React from "react";
+import { FullPage, Slide } from "react-full-page";
+import FirstMain from "../components/mains/FirstMain";
+import SecondMain from "../components/mains/SecondMain";
+import ThirdMain from "../components/mains/ThirdMain";
+import MainFooter from "../components/mains/MainFooter";
+import "../styles/style.main.css";
 
-export default function Main() {
-  return <div>Main</div>;
+function Main() {
+  return (
+    <FullPage controls controlsProps={{ className: "slide-navigation" }}>
+      <Slide>
+        <FirstMain />
+      </Slide>
+      <Slide>
+        <SecondMain />
+      </Slide>
+      <Slide>
+        <ThirdMain />
+      </Slide>
+      <Slide>
+        <MainFooter />
+      </Slide>
+    </FullPage>
+  );
 }
+
+export default Main;

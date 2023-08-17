a00fb073be4c49cf9aa50f382c5c97e577095121
chihoonahn0319 <clgnseowkd@naver.com>
2023-08-17 13:53:38 +0900

    feat: 레이아웃 적용 ,페이지 버튼이동 링크 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { Routes, Route, Link } from "react-router-dom";
 import Detail from "./pages/Detail";
 import Login from "./pages/Login";
+import Layout from "./common/Layout";
 import Signup from "./pages/Signup";
+import Home from "./pages/Home";
 import Mypage from "./pages/Mypage";
 import Main from "./pages/Main";
 import PostCreate from "./pages/PostCreate";
@@ -12,17 +14,18 @@ import Aboutus from "./pages/Aboutus";
 function App() {
   return (
     <Routes>
-      {/* <Route element={<Layout />}> */}
-      <Route path="/" element={<Main />} />
-      <Route path="/detail/:id" element={<Detail />} />
-      <Route path="/create" element={<PostCreate />} />
-      <Route path="/edit/:id" element={<PostEdit />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/mypage/:id" element={<Mypage />} />
-      <Route path="/community" element={<Community />} />
-      <Route path="/aboutus" element={<Aboutus />} />
-      {/* </Route> */}
+      <Route element={<Layout />}>
+        <Route path="/" element={<Main />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/detail/:id" element={<Detail />} />
+        <Route path="/create" element={<PostCreate />} />
+        <Route path="/edit/:id" element={<PostEdit />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/mypage/:id" element={<Mypage />} />
+        <Route path="/community" element={<Community />} />
+        <Route path="/aboutus" element={<Aboutus />} />
+      </Route>
       <Route
         path="*"
         element={
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Main() {
-  return <div>Main</div>;
+  return (
+    <div>
+      <Link to="/Home" style={{ color: "black", textDecoration: "none" }}>
+        홈으로 이동하기
+      </Link>
+      <div>Main</div>
+    </div>
+  );
 }

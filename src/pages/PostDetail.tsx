dc5ba8c987b870cb635bc9f656824cf192182cb7
leesoojin <leesoojin800@gmail.com>
2023-08-23 15:30:11 +0900

    feat : "공고 얼마 안 남은 유기견" slider 기능

--- src/pages/PostDetail.tsx
+++ src/pages/PostDetail.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import { useMutation, useQuery, useQueryClient } from "react-query";
+import { useQuery } from "react-query";
 import axios from "axios";
 
 interface Post {
@@ -14,8 +14,7 @@ interface PostDetailProps {
 }
 
 export default function PostDetail({ posts }: PostDetailProps) {
-  const queryClient = useQueryClient();
-  const { id } = useParams<{ id: string }>();
+  const { id } = useParams<{ id?: string }>();
 
   const { data, isLoading, isError, error } = useQuery(
     ["posts", id],
@@ -24,25 +23,24 @@ export default function PostDetail({ posts }: PostDetailProps) {
       return response.data;
     }
   );
-  console.log("data", data);
 
-  // const selectedPost = posts.find((post) => post.id === Number(id));
-  // console.log("디테일", selectedPost);
+  if (isLoading) {
+    return <div>로딩 중 ...</div>;
+  }
 
-  // if (isLoading) {
-  //   return <div>로딩 중 ...</div>;
-  // }
-  // if (isError) {
-  //   return <div>{error.message}</div>;
-  // }
-  // if (!data) {
-  //   return <div>게시물을 찾을 수 없습니다.</div>;
-  // }
+  if (isError) {
+    return <div>{(error as Error).message}</div>;
+  }
+
+  if (!data) {
+    return <div>게시물을 찾을 수 없습니다.</div>;
+  }
+
   return (
     <div>
-      <h2>게시글 디테일</h2>
-      <h3>{data.title}</h3>
-      <p>{data.content}</p>
+      <h2>디테일 페이지</h2>
+      <h3>{data?.title}</h3>
+      <p>{data?.content}</p>
     </div>
   );
 }

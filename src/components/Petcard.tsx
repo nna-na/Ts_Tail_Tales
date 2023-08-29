54bf66bdf7693cb4e822e85e195a906ebc099fa7
chihoonahn0319 <clgnseowkd@naver.com>
2023-08-29 20:56:16 +0900

    feat:팻 카드,좋아요버튼,컴포넌트 분리완료 마이페이지완료/무한렌더링 해결X

---
+++ src/components/Petcard.tsx
@@ -0,0 +1,117 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { formatDate, AnimalShelter } from "../api/fetchData";
+import styled from "styled-components";
+import FavoriteButton from "./FavoriteButton ";
+import { supabase } from "../supabase";
+
+export interface PetCardProps {
+  item: AnimalShelter;
+
+  //   animalId: string;
+}
+
+function PetCard({ item }: PetCardProps) {
+  const navigate = useNavigate();
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data: userData, error: userError } =
+          await supabase.auth.getUser();
+
+        if (userError) {
+          console.error("Error getting user:", userError);
+          return;
+        }
+
+        const user = userData?.user;
+        const userId = user?.id;
+
+        if (!userId) {
+          console.error("User ID not found.");
+          return;
+        }
+
+        // ------------
+
+        // 서버에서 즐겨찾기 상태 가져오기
+        const { data: existingFavorites, error: existingFavoritesError } =
+          await supabase
+            .from("favorites")
+            .select()
+            .eq("userId", userId)
+            .eq("animalId", item.ABDM_IDNTFY_NO);
+
+        if (existingFavoritesError) {
+          console.error(
+            "Error fetching existing favorites:",
+            existingFavoritesError
+          );
+          return;
+        }
+
+        setIsFavorite(existingFavorites && existingFavorites.length > 0);
+      } catch (error) {
+        console.error("Error fetching favorites:", error);
+      }
+    };
+
+    fetchData();
+  }, [item.ABDM_IDNTFY_NO]);
+
+  const handleToggleFavorite = () => {
+    setIsFavorite((prevIsFavorite) => !prevIsFavorite);
+  };
+
+  console.log("isFavorite", isFavorite);
+
+  return (
+    <Box>
+      <div
+        onClick={() =>
+          navigate(`/detail/${item.ABDM_IDNTFY_NO}`, {
+            state: { item },
+          })
+        }
+      >
+        <FavoriteButton
+          item={item}
+          isLoggedIn={true}
+          isFavorite={isFavorite}
+          onToggleFavorite={handleToggleFavorite}
+        />
+
+        <p>고유 번호 : {item.ABDM_IDNTFY_NO}</p>
+        <PetImg src={item.IMAGE_COURS} alt="Pet Thumbnail" />
+        <p>접수 일지 : {formatDate(item.RECEPT_DE)}</p>
+        <p>품종 : {item.SPECIES_NM}</p>
+        <p>성별 : {item.SEX_NM}</p>
+        <p>발견장소 : {item.DISCVRY_PLC_INFO} </p>
+        <p>특징: {item.SFETR_INFO}</p>
+        <p>상태: {item.STATE_NM}</p>
+        <p>보호 주소:{item.SIGUN_NM} </p>
+      </div>
+    </Box>
+  );
+}
+
+export default PetCard;
+
+const PetImg = styled.img`
+  width: 100%;
+  height: 280px;
+  max-width: 400px;
+  object-fit: cover;
+`;
+
+const Box = styled.div`
+  padding: 10px 10px 10px 10px;
+  border: 1px solid black;
+  width: calc(33.33% - 10px);
+  padding: 10px;
+  margin-bottom: 20px;
+  flex: 0 0 calc(33.33% - 10px);
+  box-sizing: border-box;
+`;
--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,14 +1,23 @@
-// CustomSlider.js
-import React from "react";
+import React, { useState } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { AnimalShelter } from "../api/fetchData"; // AnimalShelter 또는 실제 데이터 타입에 맞게 import 해주세요.
-import styled from "styled-components"; // styled-components를 import 해주세요.
+import { AnimalShelter } from "../api/fetchData";
+import styled from "styled-components";
+import PetCard from "./Petcard";
 
 interface CustomSliderProps {
   items: Array<AnimalShelter>;
 }
+
+const StyledSlider = styled(Slider)`
+  .slick-slide > div {
+    width: 300%;
+    padding: 10px;
+    margin-bottom: 10px;
+    box-sizing: border-box;
+  }
+`;
 
 function CustomSlider({ items }: CustomSliderProps) {
   const settings = {
@@ -20,42 +29,14 @@ function CustomSlider({ items }: CustomSliderProps) {
   };
 
   return (
-    <Slider {...settings}>
-      {items.map(
-        (
-          item: AnimalShelter // 'nearingDeadline' 대신 'items'를 사용합니다.
-        ) => (
-          <Box key={item.ABDM_IDNTFY_NO}>
-            <p>고유 번호 : {item.ABDM_IDNTFY_NO}</p>
-            <PetImg src={item.IMAGE_COURS} alt="Pet Thumbnail" />
-            <p>접수 일지 : {item.RECEPT_DE}</p>{" "}
-            {/* 'formatDate' 함수를 사용하지 않습니다. */}
-            <p>품종 : {item.SPECIES_NM}</p>
-            <p>성별 : {item.SEX_NM}</p>
-            <p>발견장소 : {item.DISCVRY_PLC_INFO} </p>
-            <p>특징: {item.SFETR_INFO}</p>
-            <p>상태: {item.STATE_NM}</p>
-            <p>보호 주소:{item.SIGUN_NM} </p>
-          </Box>
-        )
-      )}
-    </Slider>
+    <StyledSlider {...settings}>
+      {items.map((item: AnimalShelter) => (
+        <div key={item.ABDM_IDNTFY_NO}>
+          <PetCard item={item} />
+        </div>
+      ))}
+    </StyledSlider>
   );
 }
 
 export default CustomSlider;
-
-const Box = styled.div`
-  border: 1px solid black;
-  width: calc(33.33% - 10px);
-  padding: 10px;
-  margin-bottom: 20px;
-  flex: 0 0 calc(33.33% - 10px);
-  box-sizing: border-box;
-`;
-
-const PetImg = styled.img`
-  width: 100%;
-  height: auto;
-  max-width: 400px;
-`;

3819b732e2b89efcd345a6b55653a496a91ee463
jinny <ertmiu96@gmail.com>
2023-08-30 17:12:04 +0900

    design login

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,6 +1,8 @@
 import React, { useState, FormEvent } from "react";
 import { supabase } from "../supabase";
 import { User } from "@supabase/supabase-js";
+import { styled } from "styled-components";
+import { Link } from "react-router-dom";
 
 function SignUp() {
   const [email, setEmail] = useState("");
@@ -11,26 +13,29 @@ function SignUp() {
     e.preventDefault();
 
     try {
-      const { data: signUpData, error: signUpError } = await supabase.auth.signUp({
-        email,
-        password,
-        options: {
-          data: {
-            user_name: nickname,
+      const { data: signUpData, error: signUpError } =
+        await supabase.auth.signUp({
+          email,
+          password,
+          options: {
+            data: {
+              user_name: nickname,
+            },
           },
-        },
-      });
+        });
 
       if (!signUpError) {
         // 회원 가입 성공 시 추가 정보 업데이트
         if (signUpData?.user) {
           // .user 프로퍼티 확인
-          const { data: profileData, error: profileError } = await supabase.from("profiles").upsert([
-            {
-              id: signUpData.user.id as string,
-              nickname,
-            },
-          ]);
+          const { data: profileData, error: profileError } = await supabase
+            .from("profiles")
+            .upsert([
+              {
+                id: signUpData.user.id as string,
+                nickname,
+              },
+            ]);
 
           if (profileError) {
             alert("회원 정보 추가 중 오류가 발생했습니다.");
@@ -49,25 +54,115 @@ function SignUp() {
   };
 
   return (
-    <div>
-      <h1>회원 가입</h1>
+    <SignupContainer>
       <form onSubmit={signupHandler}>
-        <div>
-          <label htmlFor="email">이메일:</label>
-          <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-        </div>
-        <div>
-          <label htmlFor="password">비밀번호:</label>
-          <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        </div>
-        <div>
-          <label htmlFor="nickname">닉네임:</label>
-          <input type="text" id="nickname" value={nickname} onChange={(e) => setNickname(e.target.value)} />
-        </div>
-        <button type="submit">가입</button>
-      </form>
-    </div>
+        <InputContainer>
+          <div>
+            <InputLabel>이메일</InputLabel>
+
+            <InputBox
+              type="email"
+              id="email"
+              placeholder="이메일"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <InputLabel>비밀번호</InputLabel>
+
+            <InputBox
+              type="password"
+              id="password"
+              placeholder="비밀번호"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div>
+            <InputLabel>닉네임</InputLabel>
+
+            <InputBox
+              type="text"
+              id="nickname"
+              placeholder="닉네임"
+              value={nickname}
+              onChange={(e) => setNickname(e.target.value)}
+            />
+          </div>
+          <StButton type="submit">회원가입</StButton>
+          <NoAccountMessage>
+            계정이 있으신가요?
+            <Link to="/login">로그인</Link>
+          </NoAccountMessage>
+        </InputContainer>
+      </form>{" "}
+    </SignupContainer>
   );
 }
 
+const SignupContainer = styled.div`
+  margin: auto;
+  background-color: #fff;
+  padding: 20px;
+  width: 420px;
+  height: 700px;
+  border-radius: 10px;
+`;
+
+const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: 1px solid #9f9a9a;
+  padding: 40px;
+  margin-top: 20px;
+  border-radius: 10px;
+`;
+const InputLabel = styled.div`
+  margin: 5px 5px 5px 5px;
+  font-weight: bold;
+  justify-content: center;
+`;
+
+const InputBox = styled.input`
+  width: 282px;
+  height: 30px;
+  margin: 5px 5px 10px;
+  padding-left: 10px;
+  font-size: 15px;
+  display: inline-block;
+  outline: none;
+
+  &:focus {
+    border: 2px solid #333;
+    border-radius: 3px;
+  }
+`;
+
+const StButton = styled.button`
+  width: 300px;
+  height: 50px;
+  margin: 30px;
+  cursor: pointer;
+  font-size: 15px;
+  border-radius: 3px;
+  background-color: #333;
+  border: #333;
+  color: white;
+`;
+const NoAccountMessage = styled.div`
+  margin-top: 10px;
+  font-size: 14px;
+  color: #555;
+
+  a {
+    color: #333;
+    text-decoration: underline;
+
+    &:hover {
+      color: #555;
+    }
+  }
+`;
 export default SignUp;

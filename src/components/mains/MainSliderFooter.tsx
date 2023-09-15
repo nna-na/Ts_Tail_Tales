cfcb64a9cb73400c066ee1c0fffffedfa8e9b70b
leesoojin <leesoojin800@gmail.com>
2023-09-15 22:03:15 +0900

    fix : 사진 문제 -> footer 밖으로 꺼내기

--- src/components/mains/MainSliderFooter.tsx
+++ src/components/mains/MainSliderFooter.tsx
@@ -13,7 +13,7 @@ function MainSliderFooter() {
       name: "이예지",
       githubLink: "https://github.com/nna-na",
       blogLink: "https://velog.io/@yellog",
-      imageSrc: "/image/footer/LYJ.jpg",
+      imageSrc: "/image/LYJ.jpg",
     },
     {
       name: "안치훈",
